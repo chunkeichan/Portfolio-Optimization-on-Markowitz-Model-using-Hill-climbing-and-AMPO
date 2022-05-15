43c22a515ede1eb0335232e3af3ba54be86c847f
Chan Chun Kei <chunkeichan1901@gmail.com>
2022-05-15 11:26:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from ampo_test import ampoTest
 import time
 from params import stocklist, bound, max_iters, bounds, n_iterations, step_size, input_sol
-from sharp_ratio_function import findSharpratio, inputSharpratio, verifySharpratio
+from sharpe_ratio_function import findSharperatio, inputSharperatio, verifySharperatio
 from hill_climbing import hillclimbing
 from numpy.random import seed
 
@@ -12,7 +12,7 @@ def ampo(objective):
     best_solution = ampoTest(objective, len(
         stocklist), bound=bound, max_iters=max_iters)
     print('Best solution:' + str(best_solution))
-    print('Best fitness:' + str(findSharpratio(best_solution)))
+    print('Best fitness:' + str(findSharperatio(best_solution)))
 
     print("End time: %s" % time.strftime("%d/%m/%Y, %H:%M:%S"))
 
@@ -26,16 +26,16 @@ def hillClimb(objective):
         objective, bounds, n_iterations, step_size, len(
             stocklist))
     print('Best solution:' + str(best_solution))
-    print('Best fitness:' + str(findSharpratio(best_solution)))
+    print('Best fitness:' + str(findSharperatio(best_solution)))
 
     print("End time: %s" % time.strftime("%d/%m/%Y, %H:%M:%S"))
 
 
 def getData(input_sol):
-    value_sharpratio, value_return, value_risk = verifySharpratio(
+    value_sharperatio, value_return, value_risk = verifySharperatio(
         input_sol, rec=1)
-    print("Sharp Ratio, Return and Risk for the input solution are {}, {} and {} respectively.".format(
-        value_sharpratio, value_return, value_risk))
+    print("Sharpe Ratio, Return and Risk for the input solution are {}, {} and {} respectively.".format(
+        value_sharperatio, value_return, value_risk))
 
 
 if __name__ == "__main__":
@@ -43,7 +43,7 @@ if __name__ == "__main__":
     # Main
 
     # Selected objective function
-    objective = inputSharpratio
+    objective = inputSharperatio
 
     # 1 - Use AMPO to find the best proportions
     ampo(objective)
